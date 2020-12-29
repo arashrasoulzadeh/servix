380265836ca706771a80c24e873a6b400a2560a2
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2020-12-29 18:12:21 +0330

    Config File Cache

--- config/parser.go
+++ config/parser.go
@@ -7,15 +7,18 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var configFileCache []byte
+
 type ConfigFile struct {
 	Redis  Redis    `yaml:"redis"`
 	Config []Config `yaml:"config"`
 }
 type Config struct {
-	Root   string `yaml:"root"`
-	Server string `yaml:"server"`
-	Port   int    `yaml:"port"`
-	Index  string `yaml:"index"`
+	Root      string `yaml:"root"`
+	Server    string `yaml:"server"`
+	Port      int    `yaml:"port"`
+	Instances int    `yaml:"instances"`
+	Index     string `yaml:"index"`
 }
 
 type Redis struct {
@@ -24,9 +27,21 @@ type Redis struct {
 	Password string `yaml:"password"`
 }
 
+func GetConfigFile() ([]byte, error) {
+	if configFileCache == nil {
+		yamlFile, err := ioutil.ReadFile("config.yaml")
+		configFileCache = yamlFile
+		return yamlFile, err
+	} else {
+		return configFileCache, nil
+
+	}
+}
+
 func (c *ConfigFile) GetConfig() *ConfigFile {
 
-	yamlFile, err := ioutil.ReadFile("config.yaml")
+	// yamlFile, err := ioutil.ReadFile("config.yaml")
+	yamlFile, err := GetConfigFile()
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
 	}
